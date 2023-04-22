a002394e23cc4846fbd093f7f1ee422b870f0e8c
Alice <yummy@alice.io>
2023-04-21 19:27:53 -0700

    notes

--- src/index.jsx
+++ src/index.jsx
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client';
 import Home from './components/Home';
 import songs from './songs.json';
 
+import "./styles.css";
+
 const container = document.getElementById('app');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<Home songs={songs} />);
